8b9970775d6dae724b07b72a4a76c92fdb45bd15
James Paul Mason <jmason86@gmail.com>
2019-04-06 15:42:54 -0400

    Initial Commit

---
+++ Decision Decider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Decision Decider
+//
+//  Created by James Paul Mason on 2019-04-06.
+//  Copyright © 2019 James Paul Mason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
